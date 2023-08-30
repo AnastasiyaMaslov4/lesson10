e6e6312173c9250af111421d12e8d73f47cc0e52
Anastasiya <108454069+AnastasiyaMaslov4@users.noreply.github.com>
2023-08-30 15:13:14 +0300

    Add files via upload

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <h2>{{ product.name }}</h2>
+    <p>Price: {{ formattedPrice }}</p>
+    <p v-if="product.available">Available</p>
+    <p v-else>Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+  data() {
+    return {
+      product: {name: 'Product Name', price: 10, available: true},
+    }
+  },
+  computed: {
+    formattedPrice() {
+      return '$' + this.product.price;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .card {
+    width: 400px;
+    border: 1px solid gray;
+  }
+
+</style>
